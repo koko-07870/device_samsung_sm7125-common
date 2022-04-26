fd052c6f8916cd3b6acb27f5295773c535bbb132
Lucchetto <lucchetto.tie@live.com>
2022-04-25 20:56:35 -0400

    sm7125-common: Create dummy libqti-perfd-client
    * This way, proprietary perfd blobs can be removed without breaking
      fingerprint
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- common.mk
+++ common.mk
@@ -329,7 +329,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
-    android.hardware.power@1.2.vendor
+    android.hardware.power@1.2.vendor \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
